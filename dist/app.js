25ed378a6ccb5b1fcf49c8b15da091d28c408978
Rupok-Koiry <koiry.rupok@gmail.com>
2024-07-13 21:48:39 +0600

    first commit

--- dist/app.js
+++ dist/app.js
@@ -6,9 +6,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const cookie_parser_1 = __importDefault(require("cookie-parser"));
 const cors_1 = __importDefault(require("cors"));
 const express_1 = __importDefault(require("express"));
-const globalErrorhandler_1 = __importDefault(require("./app/middlewares/globalErrorhandler"));
-const notFound_1 = __importDefault(require("./app/middlewares/notFound"));
-const routes_1 = __importDefault(require("./app/routes"));
 const app = (0, express_1.default)();
 // Middleware for parsing JSON bodies
 app.use(express_1.default.json());
@@ -16,10 +13,7 @@ app.use(express_1.default.json());
 app.use((0, cookie_parser_1.default)());
 // Middleware for enabling Cross-Origin Resource Sharing (CORS) for specified origins
 app.use((0, cors_1.default)());
-// Route handlers for API endpoints prefixed with /api/v1
-app.use('/api', routes_1.default);
-// Middleware for handling global errors
-app.use(globalErrorhandler_1.default);
-// Middleware for handling 404 - Not Found errors
-app.use(notFound_1.default);
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
 exports.default = app;
--- dist/server.js
+++ dist/server.js
@@ -12,9 +12,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const mongoose_1 = __importDefault(require("mongoose"));
 const app_1 = __importDefault(require("./app"));
-const config_1 = __importDefault(require("./app/config"));
 let server;
 /**
  * Main function to establish database connection and start the server.
@@ -22,11 +20,10 @@ let server;
 function main() {
     return __awaiter(this, void 0, void 0, function* () {
         try {
-            // Connect to the MongoDB database using the provided URL from the configuration.
-            yield mongoose_1.default.connect(config_1.default.DATABASE_URL);
+            const PORT = process.env.PORT || 8000;
             // Start the server and listen on the specified port from the configuration.
-            server = app_1.default.listen(config_1.default.PORT, () => {
-                console.log(`App is listening on port ${config_1.default.PORT}`);
+            server = app_1.default.listen(PORT, () => {
+                console.log(`App is listening on port ${PORT}`);
             });
         }
         catch (err) {
